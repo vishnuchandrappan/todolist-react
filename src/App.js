6b6c6a7cac05c34139eb1135c84b349d18769291
vishnuchandrappan <cvishnu70@gmail.com>
2019-12-26 22:07:57 +0530

    added react-toastify

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 import React, { Component } from "react";
-import uuid from 'uuid';
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import uuid from "uuid";
 import AddTodo from "./components/AddTodo.js";
 import Header from "./components/layouts/Header";
 import Todos from "./components/Todos";
@@ -12,12 +14,13 @@ export class App extends Component {
   };
 
   componentDidMount() {
-    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
-      .then((response) => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=5")
+      .then(response => {
         this.setState({
           todos: response.data
         });
-      })
+      });
   }
   // Toggle Complete
   toggleComplete = id => {
@@ -25,6 +28,7 @@ export class App extends Component {
       todos: this.state.todos.map(todo => {
         if (todo.id === id) {
           todo.completed = !todo.completed;
+          toast.info(todo.completed ? "Marked as Done" : "Marked as not complete")
         }
         return todo;
       })
@@ -35,6 +39,7 @@ export class App extends Component {
     this.setState({
       todos: [...this.state.todos.filter(todo => todo.id !== id)]
     });
+    toast.info("Todo Deleted");
   };
 
   addTodo = title => {
@@ -43,7 +48,7 @@ export class App extends Component {
     newTitles.forEach(newTitle => {
       if (newTitle.trim()) {
         newTodos.push({
-          id:uuid.v4(),
+          id: uuid.v4(),
           title: newTitle,
           completed: false
         });
@@ -52,6 +57,7 @@ export class App extends Component {
     this.setState({
       todos: this.state.todos.concat(newTodos)
     });
+    toast.info("Todo Added");
   };
 
   render() {
@@ -66,6 +72,11 @@ export class App extends Component {
             deleteTodo={this.deleteTodo}
           />
         </div>
+        <ToastContainer
+          position="bottom-center"
+          autoClose={3000}
+          hideProgressBar={true}
+        />
       </React.Fragment>
     );
   }
